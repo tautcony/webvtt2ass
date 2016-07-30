99818295172a96102cd5677e10e80b6a7ba4f11e
tautcony <tautcony@gmail.com>
2016-07-30 18:44:41 +0800

    Fix time format and text escape.

--- src.js
+++ src.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var parser = new (require('./lib/webvtt/parser.js'))();
 var TimeSpan = require('./lib/timespan');
+var _ = require('Underscore');
 
 if (!String.prototype.format) {
   String.prototype.format = function() {
@@ -20,9 +21,10 @@ console.log("﻿[Script Info]\nScriptType: v4.00+\nPlayResX: 1920\nPlayResY: 108
 console.log("[V4+ Styles]\nFormat: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\nStyle: Default,方正准圆_GBK,50,&H00FFFFFF,&H000000FF,&H00000000,&H00000000,0,0,0,0,100,100,0,0,1,2,0.5,2,10,10,10,1\n");
 console.log("[Events]\nFormat: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text");
 ret.cues.forEach(function (item, index, array) {
-    var beginTime = new TimeSpan(item.startTime*1000).toString();
-    var endTime = new TimeSpan(item.endTime*1000).toString();
+    var beginTime = new TimeSpan(item.startTime*1000).assFormat();
+    var endTime = new TimeSpan(item.endTime*1000).assFormat();
     var text = item.text;
     text = text.replace('\n', '\\N');
+    text = _.unescape(text);
     console.log("Dialogue: 0,{0},{1},Default,,0,0,0,,{2}".format(beginTime, endTime, text));
 });
